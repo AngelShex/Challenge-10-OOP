df012b955a7b93185de47db42f16f76cc56cd833
Angel Shelton <angelshelton@Angels-Air.local.tld>
2024-03-06 15:02:16 -0800

    input cost functions within the index JS file

---
+++ index.js
@@ -0,0 +1,4 @@
+// imports the modules and also attaches SVG string with this function
+const filesystem = require('./node_modules/graceful-fs/graceful-fs')
+const inquirer = require = require("inquirer");
+const {Circle, Square, Triangle} = require("./lib/shapes");
